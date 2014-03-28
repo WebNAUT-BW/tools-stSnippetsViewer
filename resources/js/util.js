d104816231589c770a1aa76cf110e0af78230324
Kotaro Shimoda <k_shimoda@beeworks.co.jp>
2014-03-28 11:03:11 +0900

    ざっくり動き完成。

--- resources/js/util.js
+++ resources/js/util.js
@@ -1,7 +1,6 @@
 $(function () {
 	if (window.File) {
 		// File APIに関する処理を記述
-		//window.alert("File APIが実装されてます。");
 		document.getElementById("drop").addEventListener("drop", onDrop, false);
 	} else {
 		window.alert("本ブラウザではFile APIが使えません");
@@ -11,15 +10,12 @@ $(function () {
 // Drop領域にドロップした際のファイルのプロパティ情報読み取り処理
 function onDrop(event) {
 	var files = event.dataTransfer.files;
-	var disp = document.getElementById("disp");
-	disp.innerHTML = "";
 
 	for (var i = 0; i < files.length; i++) {
 		var f = files[i];
 		// ファイル名とサイズを表示
 		//disp.innerHTML += "ファイル名 :" + f.name + "ファイルの型:" + f.type + "ファイルサイズ:" + f.size / 1000 + " KB " + "<br />";
 		readText(f.name,files[i]);
-		//readText(f.name,f.type);
 	}
 	event.preventDefault();
 }
@@ -29,30 +25,48 @@ function onDragOver(event) {
 }
 
 function readText(name,input) {
+	// File APIを利用できるかをチェック
+	if (window.File) {
 
+		// ファイル読み込みの準備
+		var reader = new FileReader();
 
-    // File APIを利用できるかをチェック
-    if (window.File) {
-      // 指定されたファイルを取得
-      //var input = document.querySelector('#file').files[0];
-      // ファイル読み込みの準備（1）
-      var reader = new FileReader();
-      // ファイルの読み込みに成功したら、その内容を<div id="result">に反映（2）
-      reader.addEventListener('load', function(e) {
-        var output = reader.result.replace(/(\n|\r)/g, '<br />');
+		// ファイルの読み込みに成功した後の処理
+		reader.addEventListener('load', function(e) {
+			//var fileContent = reader.result.replace(/(\n|\r)/g, '<br />');
 
+			//ファイルの中身格納
+			var fileContent = reader.result;
 
-        var outputName = '<tr><td>' + name + '</td>';
-        var outputContent = '<td>' + output + '</td><td></td></tr>';
-        console.log('outputName=' + outputName);
-        console.log('outputContent=' + outputContent);
+			//fileContent内の文字列処理：<content>の中身を取り出す、"<![CDATA["と"]]>"も合わせて削除
+			var indexTagContent = fileContent.indexOf("<content>"); 
+			var indexTagContentEnd = fileContent.indexOf("</content>"); 
+			var txtContent = fileContent.substring(indexTagContent,indexTagContentEnd).replace("<![CDATA[", "").replace("]]>", "").replace("<content>", "").replace("</content>", "");
 
-        $('#outputTable').append(outputName + outputContent);
-      }, true);
-      // ファイルの内容をテキストとして取得（3）
-      reader.readAsText(input, 'UTF-8');
-    }
+			//txtContentをそのまま出力するとタグとして生成されるので、ASCII文字に変換
+			txtContent_length = txtContent.length;
+			var txtContentAs = "";
+			for(var i=0;i<txtContent_length;i++){
+				txtContentAs = txtContentAs + "&" + "#" + txtContent.charCodeAt(i) + ";";
+			}
 
+			//fileContent内の文字列処理：<tabTrigger>の中身を取り出す
+			var indexTabTrigger = fileContent.indexOf("<tabTrigger>"); 
+			var indexTabTriggerEnd = fileContent.indexOf("</tabTrigger>"); 
+			var txtTabTrigger = fileContent.substring(indexTabTrigger,indexTabTriggerEnd).replace("<tabTrigger>", "").replace("</tabTrigger>", "");
 
+			//出力用HTMLの文字列を作成
+			var outputName = '<td>' + name + '</td>';
+			var outputContent = '<td><pre>' + txtContentAs + '</pre></td>';
+			var outputTabTrigger = '<td>' + txtTabTrigger + '</td>';
+			var outputAll = '<tr>' + outputName + outputContent + outputTabTrigger + '</tr>';
+
+			//HTML出力
+			$('#outputTable').append(outputAll);
+		}, true);
+
+		// ファイルの内容をテキストとして取得（3）
+		reader.readAsText(input, 'UTF-8');
+	}
 }
 
